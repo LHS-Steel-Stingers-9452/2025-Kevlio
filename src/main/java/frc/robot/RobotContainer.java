f41652ca120594c7d9e518d40dcb70bf1d5be72d
frijolillos <covarrubiaslizbeth9@gmail.com>
2025-02-23 14:12:56 -0800

    drive practice ready!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -117,9 +117,9 @@ public class RobotContainer {
             () ->
                 drive
                     .withVelocityX(
-                        joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                        -joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
                     .withVelocityY(
-                        joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                        -joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
                     .withRotationalRate(
                         -joystick.getRightX()
                             * MaxAngularRate) // Drive counterclockwise with negative X (left)
